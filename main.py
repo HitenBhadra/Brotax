7c40a8727e1b1eba657e75b4f66eae16148f556a
HitenBhadra <67404448+HitenBhadra@users.noreply.github.com>
2023-06-28 02:00:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -79,11 +79,17 @@ st.write("Total tax new regime:", new_tax)
 #docs
 st.header('What is Income Tax Calculator')
 st.markdown('''
-An Income-tax calculator is an online tool that helps to evaluate taxes based on a person’s income once the Union Budget for the year is announced. Individuals falling under the taxable income bracket are liable to pay a specific portion of their net annual income as tax. Income tax can be paid either as tax deducted at source while disbursement of monthly salary, or through the income tax returns portal managed by the Central Board of Direct Taxes (CBDT). The provision for online payment of taxes is to ensure individuals pay their stipulated dues on any earnings generated from other sources.
+An Income-tax calculator is an online tool that helps to evaluate taxes based on a person’s income once the Union Budget for the year is announced. Individuals falling under the taxable income bracket are liable to pay a specific portion of their net annual income as tax. Income tax can be paid either as tax deducted at source while disbursement of monthly salary, or through the income tax returns portal managed by the Central Board of Direct Taxes (CBDT). The provision for online payment of taxes is to ensure individuals pay their stipulated dues on any earnings generated from other sources.The IT calculator given on this page is aligned with the updates announced in the Union Budget for FY 2023-24 and AY 2024-25.''')
 
-The IT calculator given on this page is aligned with the updates announced in the Union Budget for FY 2023-24 and AY 2024-25.''')
+st.header('About us')
+st.markdown('''This income tax calculator project was created by Hiten and Devesh, two students in XII-'A' at KV No.1 Manglore Panambur. The project was guided by Sunil sir, a computer science teacher at the school.''')
 
-st.write('Project by Hiten & Devesh')
+st.header('The Goal')
+st.markdown('''The goal of the project was to create a simple and easy-to-use income tax calculator that could be used by anyone. The calculator takes into account the latest Indian income tax slabs and deductions, and it provides a clear and concise output.''')
+
+st.header('Tech used')
+st.markdown('''Pythom 3.7 \nStreamlit \nMySQL''')
+
 
 
 
